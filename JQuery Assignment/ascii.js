6567d5e02621100b11b4e64f8f254ac678bfe4dd
BlessingsNaggayi <blessingsimelda.naggayi@miu.edu>
2023-05-10 19:18:10 -0500

    initially

--- JQuery Assignment/ascii.js
+++ JQuery Assignment/ascii.js
@@ -1,16 +1,48 @@
+"use sctrict";
+
+
+
 $(document).ready(function () {
 
-    $("#startButton").click(function () {
+    var animationSelect = $('#animation-select');
+    var animationInput= $('animationDisplay');
+    var fontSizeSelect =$('#size');
+    var animation = $('animation');
+    var startButton =$('#play');
+    var stopButton = $('stopButton');
+    var frames=[];
+    var currentFrame = 0;
+    var animationInterval;
 
-        alert("this works!");
+  $animationSelect.on('change', function() {
+    var selectedAnimation = $animationSelect.val();
+    $animationInput.val(selectedAnimation);
+  });
+  
 
-    });
+  $fontSizeSelect.on('change', function() {
+    var selectedFontSize = $fontSizeSelect.val();
+    $animation.css('font-size', selectedFontSize + 'px');
+  });
+
+  $stopButton.prop("disabled", true);
+
+  $startButton.click(function(){
+    
+    $stopButton.prop("disabled", false);
+
+    if($('#speed').prop("checked")){
+       animateSlow(50);
+    }else{
+        animateSlow(250);
+    }
+
+    
 
 
-    $("#size").select(function(){
 
-        
+  }
+  )
 
-    })
 
 });
